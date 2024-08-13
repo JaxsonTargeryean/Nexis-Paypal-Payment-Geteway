a2da433d6e112e75842ddfc066ccb60e91a4d93c
JJnr83x <hf2498dfjhncds@outlook.com>
2024-08-13 18:33:41 +0300

    Upload Files

---
+++ Paypal-Payment/config.js
@@ -0,0 +1,3 @@
+export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
+export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
+export const PAYPAL_API = 'https://api-m.sandbox.paypal.com';
